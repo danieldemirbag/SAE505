6799a9321d5f54e0003d403efef5681ebda09e64
danie <daniel.demirbag@uha.fr>
2023-10-12 10:40:19 +0200

    creation de compte final

--- GUItestDaniel.py
+++ GUItestDaniel.py
@@ -10,7 +10,7 @@
 
 from PyQt5 import QtCore, QtGui, QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow, QPushButton, QVBoxLayout, QWidget
-import GUItestDanielrc, sys
+import qrc, sys
 
 
 class Ui_Login(object):
